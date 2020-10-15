6ad89a31d289884bca0f88dcd044180dc131f6b4
Gemi Khong <nhatvankhong@gmail.com>
2020-10-15 11:11:28 -0500

    Add placeBid update

---
+++ src/handlers/placeBid.js
@@ -0,0 +1,38 @@
+import AWS from 'aws-sdk';
+import commonMiddleWare from "../lib/commonMiddleWare";
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+    const { id } = event.pathParameters;
+    const { amount } = event.body;
+
+    const params = {
+        TableName: process.env.AUCTIONS_TABLE_NAME,
+        Key: { id },
+        UpdateExpression: 'set highestBid.amount = :amount',
+        ExpressionAttributeValues: {
+            ':amount': amount,
+        },
+        ReturnValues: 'ALL_NEW',
+    };
+
+    let updatedAuction;
+
+    try {
+        const result = await dynamodb.update(params).promise();
+        updatedAuction = result.Attributes;
+    } catch (error) {
+        console.error(error);
+        throw new createError.InternalServerError(error);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(auction),
+    };
+}
+
+export const handler = commonMiddleWare(placeBid);
+
